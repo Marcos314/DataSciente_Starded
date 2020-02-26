6a15c214c083c2897f597b0be98e4d5b4d6042e4
Marcos314 <marcoswesley314@gmail.com>
2020-02-25 22:08:29 -0300

    class

--- python_dc/POO/class2.py
+++ python_dc/POO/class2.py
@@ -3,11 +3,11 @@
 class Livro():
 
     #método responsável por inicializar cada obj criado a partir dessa classe
-    def __init__(self):
+    def __init__(self, titulo, isbn):
 
         #atributos de cada obj criado a partir desta classe, o self é uma referencia a cada atributo criado na classe
-        self.titulo = 'O monge executivo'
-        self.isbn = 99888888
+        self.titulo = titulo
+        self.isbn = isbn
         print('Construtor chamado para criar um obj desta classe')
 
         #Pelo que entendi, toda vez q um obj for instanciado, ele vai ser criado já com titulo e isbn
@@ -16,12 +16,6 @@ class Livro():
         print('Foi criado o livro %s e ISBN %d '%(self.titulo, self.isbn))
         print('{} E {}'.format(self.titulo, self.isbn))
 
-Livro1 = Livro()
 
-Livro1.imprimi()
-
-
-
-
-
-
+Livro2 = Livro('O guia do mochileiro', 1111)
+Livro2.imprimi()
