53c30528a8907de57a6d7e08f946e2afb0dfb323
Marcos314 <marcoswesley314@gmail.com>
2020-02-29 21:14:48 -0300

    manipulando db com python

--- python_dc/python_db/db1.py
+++ python_dc/python_db/db1.py
@@ -1,1 +1,40 @@
-#Manipulando Banco de Dados em Python
+# Manipulando Banco de Dados em Python
+
+import os
+
+os.remove('escola.db') if os.path.exists('escola.db') else None
+
+import sqlite3
+
+con = sqlite3.connect('escola.db')
+
+# cursor para acessar o bd
+cs = con.cursor()
+print(type(cs))
+
+sql_create = "create table cursos " \
+             "(id integer primary key, " \
+             "titulo varchar(100) , " \
+             "categoria varchar(140))"
+
+cs.execute(sql_create)
+sql_insert = 'insert into cursos values (?, ?, ?)'
+
+rec_set = [(1000, 'Ciencia de dados','Data Science'),
+           (1001, 'Big data 1', 'big data'),
+           (1002, 'python', 'python')]
+
+for rec in rec_set:
+    cs.execute(sql_insert,rec)
+
+con.commit()
+
+sql = 'select * from cursos'
+
+cs.execute(sql)
+
+dados = cs.fetchall()
+
+
+for l in dados:
+    print(l)
